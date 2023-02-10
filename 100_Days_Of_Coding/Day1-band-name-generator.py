05e5798f89417208478f61b9225b09634ea56a56
whsaik <114804650+whsaik@users.noreply.github.com>
2023-02-10 12:30:27 +0800

    Create Day1-band-name-generator.py

---
+++ 100_Days_Of_Coding/Day1-band-name-generator.py
@@ -0,0 +1,12 @@
+# Greeting
+print("Hello, welcome to the band name generator!!!")
+
+# Getting some info about the user
+city = input("What is the city you grew up in?\n")
+pet_name = input("What is the name of your pet?\n")
+
+# Combining info to create band name
+band_name = city + " " + pet_name
+
+# Output the results
+print(f"The possible band name is '{band_name}'.")
