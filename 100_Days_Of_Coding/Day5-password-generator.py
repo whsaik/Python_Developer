e165ae95ba51010851ba31a03a0b3efa89b48018
whsaik <114804650+whsaik@users.noreply.github.com>
2023-02-13 12:20:09 +0800

    Update Day5-password-generator.py

--- 100_Days_Of_Coding/Day5-password-generator.py
+++ 100_Days_Of_Coding/Day5-password-generator.py
@@ -16,16 +16,14 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 password =[]
 
 for i in range(nr_letters):
-  x = letters[random.randint(0, len(letters)-1)]  #another way of doing: use random.choice() to randomly pick element from a list
+  x = letters[random.randint(0, len(letters)-1)]  
   password.append(x)
 
 for i in range(nr_symbols):
-  x = symbols[random.randint(0, len(symbols)-1)]
-  password.append(x)
+  password.append(random.choice(symbols)) #another way of randomly picking from list
 
 for i in range(nr_numbers):
-  x = numbers[random.randint(0, len(numbers)-1)]
-  password.append(x)
+  password.append(random.choice(numbers))
 
 #Eazy Level - Order not randomised:
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
@@ -35,10 +33,13 @@ print("".join(password))
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
 
 #Randomised the order of the password
-#another way of doing: use the random.shuffle(list) to shuffle the list
 hard_pass = []
 for k in range(len(password)):
   a = password.pop(random.randint(0, len(password)-1))
   hard_pass.append(a)
 
 print("".join(hard_pass))
+
+# Another way to randomise the list - use the shuffle method
+# random.shuffle(password)
+# print("".join(password))
