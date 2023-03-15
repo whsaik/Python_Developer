f1f1ca7362153776679f3934b8679c9e94f03910
Waihou Saik <114804650+whsaik@users.noreply.github.com>
2023-03-15 16:50:00 +0800

    error handling included
    include error handling which only allow input of alphabets

--- 100_Days_Of_Coding/026-Day26-nato-phonetic/main.py
+++ 100_Days_Of_Coding/026-Day26-nato-phonetic/main.py
@@ -1,10 +1,17 @@
 import pandas as pd
 
-#TODO 1. Create a dictionary in this format:
 nato = pd.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter:row.code for (index, row) in nato.iterrows()}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user = input("Enter a word: ")
-results = [nato_dict[letter.upper()] for letter in user]
-print(results)
+def generate_phonetic():
+    try:
+        user = input("Enter a word: ")
+        results = [nato_dict[letter.upper()] for letter in user]
+    except:
+        print("Enter only alphabets please.")
+        generate_phonetic()
+    else:
+        print(results)
+        
+    
+generate_phonetic()
